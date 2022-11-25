9b28a5be8dbb1b9deae930bc651f1a8d17f32feb
Anton <fullbushgames@gmail.com>
2022-11-25 18:21:16 +0300

    Commit for Linux IDE

---
+++ tgbot/API.cs
@@ -0,0 +1,6 @@
+﻿namespace tgbot;
+
+public static class API
+{
+    internal static string BotToken = "5344641120:AAEU5mpCP0LohEKceNBRmH1_D1ttpYQ-8RM";
+}
---
+++ tgbot/Program.cs
@@ -0,0 +1,69 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using tgbot;
+var botclient = new TelegramBotClient(API.BotToken);
+using var cts = new CancellationTokenSource();
+var receiverOptions = new ReceiverOptions
+{
+    AllowedUpdates = {}
+};
+botclient.StartReceiving(HandleUpdatesMessagesAsync, HandleErrorAsync,
+    receiverOptions,
+    cancellationToken: cts.Token);
+
+User me = await botclient.GetMeAsync();
+Console.WriteLine($"Начал прослушку : {me.Username}");
+Console.ReadLine();
+cts.Cancel();
+
+async Task HandleUpdatesMessagesAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    var message = update.Message;
+    var chatId = message.Chat.Id;
+    
+    if (message?.Text !=null)
+    {
+        await botclient.SendTextMessageAsync(chatId, $"Вас приветствует EkrimBot, что вы хотите сделать? ");
+    }
+    if (message.Type == MessageType.Document)
+    {
+        Console.WriteLine($"Получен документ {message.Document}");
+        await DownloadDocuments(botclient, message);
+        Console.WriteLine($"Документ успешно сохранен ");
+    }
+}
+
+
+Task HandleErrorAsync (ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var errorMessage = exception switch
+    {
+        ApiRequestException apiRequestException =>
+            $"Ошибка API телеграм: \n {apiRequestException.ErrorCode}\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+    Console.WriteLine(errorMessage);
+    return Task.CompletedTask;
+}
+
+async Task DownloadDocuments(TelegramBotClient botClient, Message message)
+{
+    var fileId = message.Document?.FileId;
+    string destanationPath = $@"C:\TelegrammFiles\{message.Document.FileName}";
+    
+    if (!Directory.Exists(destanationPath)) Directory.CreateDirectory(destanationPath);
+    await using FileStream fstream = new FileStream(destanationPath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+    await botclient.GetInfoAndDownloadFileAsync(fileId: fileId, destination: fstream);
+     fstream.Close();
+}
+
+
+
+
+
+
+
